4f087daeebb4c7ecce873086b78911aa409b2b98
Tunar Mahmudov <tunardev14@gmail.com>
2019-12-20 15:11:54 +0300

    parse server setup
    Back4app parse server setup

--- app/src/main/java/com/example/careering/MainActivity.java
+++ app/src/main/java/com/example/careering/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.careering;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+
+import com.parse.Parse;
+import com.parse.ParseInstallation;
+import com.parse.ParseObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        InitializeParseServer();
+    }
+
+    private void InitializeParseServer() {
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId(getString(R.string.back4app_app_id))
+                // if defined
+                .clientKey(getString(R.string.back4app_client_key))
+                .server(getString(R.string.back4app_server_url))
+                .build()
+        );
+        ParseInstallation.getCurrentInstallation().saveInBackground();
     }
 }
