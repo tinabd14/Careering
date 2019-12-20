dcb5754326c4e4c8f408ba1534060258431f1bf1
Ali Babayev <ali.babayev@ug.bilkent.edu.tr>
2019-12-20 22:25:45 +0300

    Added Drawer, Homepage, created PostScreen and DisplaySinglePost Screens

---
+++ app/src/main/java/com/example/careering/DisplaySinglePostScreen.java
@@ -0,0 +1,14 @@
+package com.example.careering;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DisplaySinglePostScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.display_single_post_screen);
+    }
+}
--- app/src/main/java/com/example/careering/HomepageScreen.java
+++ app/src/main/java/com/example/careering/HomepageScreen.java
@@ -1,22 +1,101 @@
 package com.example.careering;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
+import com.parse.Parse;
+import com.parse.ParseUser;
 
 public class HomepageScreen extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homepage_screen);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        mDrawerLayout.addDrawerListener(
+                new DrawerLayout.DrawerListener() {
+                    @Override
+                    public void onDrawerSlide(View drawerView, float slideOffset) {
+                        // Respond when the drawer's position changes
+                    }
+
+                    @Override
+                    public void onDrawerOpened(View drawerView) {
+                        TextView navTitle = (TextView) findViewById(R.id.navHeaderTitle);
+                        navTitle.setText(ParseUser.getCurrentUser().getUsername());
+                    }
+
+                    @Override
+                    public void onDrawerClosed(View drawerView) {
+                        // Respond when the drawer is closed
+                    }
+
+                    @Override
+                    public void onDrawerStateChanged(int newState) {
+                        // Respond when the drawer motion state changes
+                    }
+                }
+        );
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        if(menuItem.getTitle().toString().matches("My Profile Info")) {
+                            Intent intent = new Intent(getApplicationContext(), ProfileScreen.class);
+                            startActivity(intent);
+                        }
+                        else if(menuItem.getTitle().toString().matches("Create a Post")) {
+                            Intent intent = new Intent(getApplicationContext(), CreatePostScreen.class);
+                            startActivity(intent);
+                        }
+                        else if(menuItem.getTitle().toString().matches("Log out")) {
+                            if(ParseUser.getCurrentUser() != null) {
+                                Toast.makeText(getApplicationContext(), "Logged out", Toast.LENGTH_SHORT).show();
+                                ParseUser.logOut();
+                                Intent intent = new Intent(getApplicationContext(), LoginScreen.class);
+                                startActivity(intent);
+                            }
+                        }
+                        // close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+                        return true;
+                    }
+                });
+
     }
+
+
 
     @Override
     public void onBackPressed() {
-
         new AlertDialog.Builder(this)
                 .setIcon(android.R.drawable.ic_dialog_alert)
                 .setTitle(getApplicationContext().getResources().getString(R.string.word_quit))
