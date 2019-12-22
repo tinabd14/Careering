d3375ecc64695da5337e417e772fbe435b47d08d
Ali Babayev <ali.babayev@ug.bilkent.edu.tr>
2019-12-22 18:22:15 +0300

    created GUIs for Post screens

--- app/src/main/java/com/example/careering/CreateTestScreen.java
+++ app/src/main/java/com/example/careering/CreateTestScreen.java
@@ -3,17 +3,12 @@ package com.example.careering;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-public class CreatePostScreen extends AppCompatActivity {
+public class CreateTestScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.create_post_screen);
-    }
-
-    public void createPost(View view) {
-
+        setContentView(R.layout.create_test_screen);
     }
 }
--- app/src/main/java/com/example/careering/DisplaySinglePostScreen.java
+++ app/src/main/java/com/example/careering/DisplaySinglePostScreen.java
@@ -2,13 +2,34 @@ package com.example.careering;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class DisplaySinglePostScreen extends AppCompatActivity {
+
+    TextView postTitle;
+    TextView postDescription;
+    Button applyButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.display_single_post_screen);
+
+        postTitle = findViewById(R.id.postTitle);
+        postDescription = findViewById(R.id.postDescription);
+        applyButton = findViewById(R.id.applyButton);
+    }
+
+    public void apply(View v) {
+        applyButton.setActivated(false);
+
+
+
+        Intent intent = new Intent(getApplicationContext(), DisplayTestScreen.class);
+        startActivity(intent);
     }
 }
