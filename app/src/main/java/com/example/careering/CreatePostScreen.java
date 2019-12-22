623a5b035086fe34eb388fa810981c2754598ee2
Ali Babayev <ali.babayev@ug.bilkent.edu.tr>
2019-12-22 18:54:47 +0300

    updated Create post part

--- app/src/main/java/com/example/careering/CreatePostScreen.java
+++ app/src/main/java/com/example/careering/CreatePostScreen.java
@@ -2,18 +2,43 @@ package com.example.careering;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class CreatePostScreen extends AppCompatActivity {
+
+
+    EditText postTitle;
+    EditText postCompany;
+    EditText postPublisherName;
+    EditText postDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_post_screen);
+
+        postTitle = findViewById(R.id.createPostTitle);
+        postCompany = findViewById(R.id.createPostCompany);
+        postPublisherName = findViewById(R.id.createPostPublisherName);
+        postDescription = findViewById(R.id.createPostDescription);
+
     }
 
     public void createPost(View view) {
+        if (postTitle.getText().toString() != "" || postCompany.getText().toString() != "" ||
+                postPublisherName.getText().toString() != "" ||
+                postDescription.getText().toString() != "") {
+            Toast.makeText(this, "Please fill all fields before publishing", Toast.LENGTH_LONG).show();
+        }
+        else {
 
+            //TODO : add the post to database
+            Intent intent = new Intent(getApplicationContext(), HomepageScreen.class);
+            startActivity(intent);
+        }
     }
 }
