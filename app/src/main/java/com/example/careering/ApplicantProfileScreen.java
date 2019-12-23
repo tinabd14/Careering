b6b413f6f9eecab7aa40afe0acb678d1c9d3448b
Ali Babayev <ali.babayev@ug.bilkent.edu.tr>
2019-12-23 14:38:31 +0300

    Added applicant's profile

--- app/src/main/java/com/example/careering/ApplicantProfileScreen.java
+++ app/src/main/java/com/example/careering/ApplicantProfileScreen.java
@@ -3,12 +3,23 @@ package com.example.careering;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ApplicantProfileScreen extends AppCompatActivity {
+
+    TextView name;
+    TextView profileInfo;
+    TextView jobPosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.applicant_profile_screen);
+
+        name = findViewById(R.id.nameTextApplicant);
+        jobPosition = findViewById(R.id.positionApplicant);
+        profileInfo = findViewById(R.id.InfoApplicant);
+
     }
 }
